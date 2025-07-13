56fbb4e5cc2969a47f0cd073589f8497c66b4236
Harsh-Kumar-Upadhyay <management.harsh@gmail.com>
2025-07-13 12:29:17 +0530

    Initial commit

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+document.getElementById("option1").onclick = function() {
+    location.href = "./option1.html";
+};
+document.getElementById("option2").onclick = function() {
+    location.href = "./option2.html";
+};
