2338f2f02a0b33c36eb0f3d1e8e84c535cd015f8
Harsh Upadhyay <management.harsh@gmail.com>
2026-02-05 23:36:27 +0530

    ccc

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const GALLERY_DATA = [
         ids: [
             "1W17eEk0uZA36RgJ6Gc-AAddNgmn6uGAS",
             "1wq0JSOlEma6cVCFHSXXHJv1ESPjsoFyM",
-            "1oUYJeVoYhtMn_M69fuK11pTIKRwTMOey",
+            "1oUYJeVoYhtMn_M69fuK11pTIKRwTMOey"
         ], 
         description: "CTO with TRDDC (TCS Research) team."
     },
